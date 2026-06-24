e8ce9931af13e0c155b466287d16dfb620047f09
0-byte-dotcom <falceso.342711@batangas.sti.edu.ph>
2026-06-24 21:10:38 +0800

    Add files via upload

---
+++ ACES/script.js
@@ -0,0 +1,6 @@
+ const menuBtn = document.querySelector('.menu-btn')
+        const navlist = document.querySelector('.navlist')
+        
+        
+        menuBtn.addEventListener('click',()=>{ navlist.classList.toggle('mobile-menu')
+        })
